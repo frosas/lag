dd8eecb64188af87235fa4ee5b763e00657097ad
Francesc Rosàs <francescrosasbosque@gmail.com>
2011-12-12 19:44:20 +0100

    chart module

---
+++ scripts/chart.js
@@ -0,0 +1,35 @@
+define(function() {
+
+    var pings = (function() {
+        var pings = []
+        var max = 40
+        return {
+            add: function(ping) {
+                pings.push(ping)
+                if (pings.length > max) pings = pings.slice(1)
+            },
+            pings: function() {
+                return pings
+            }
+        }
+    })()
+
+    var update = function() {
+        var update = d3.select('#chart').selectAll('div')
+            .data(pings.pings(), function(ping) { return ping.start })
+        update.append('div')
+            .text(function(ping) { return ping.lag })
+        update.enter().append('div')
+            .style('height', function(ping) { return ping.lag + 'px' })
+            .append('div')
+                .text(function(ping) { return ping.lag })
+        update.exit().remove()
+    }
+
+    return {
+        add: function(ping) {
+            pings.add(ping)
+            update()
+        }
+    }
+})
--- scripts/main.js
+++ scripts/main.js
@@ -1,18 +1,4 @@
-require(['ping-url', 'page-title'], function(pingUrl, pageTitle) {
-
-    var pings = (function() {
-        var pings = []
-        var max = 40
-        return {
-            add: function(start, lag) {
-                pings.push({start: start, lag: lag})
-                if (pings.length > max) pings = pings.slice(1)
-            },
-            pings: function() {
-                return pings
-            }
-        }
-    })()
+require(['ping-url', 'chart', 'page-title'], function(pingUrl, chart, pageTitle) {
 
     ;(function continuousPing() {
 
@@ -23,21 +9,11 @@ require(['ping-url', 'page-title'], function(pingUrl, pageTitle) {
         var url = 'http://ajax.googleapis.com/ajax/libs/chrome-frame/1.0.2/CFInstall.min.js'
 
         pingUrl(url, function(error, ping) {
+
             if (error) {
                 console.error(error)
             } else {
-                pings.add(ping.start, ping.lag)
-
-                var update = d3.select('#chart').selectAll('div')
-                    .data(pings.pings(), function(ping) { return ping.start })
-                update.append('div')
-                    .text(function(ping) { return ping.lag })
-                update.enter().append('div')
-                    .style('height', function(ping) { return ping.lag + 'px' })
-                    .append('div')
-                        .text(function(ping) { return ping.lag })
-                update.exit().remove()
-
+                chart.add(ping)
                 pageTitle.update(ping)
             }
 
