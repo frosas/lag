ff145a1a44da88899fe0c173a1059c9b7fdd8edd
Francesc Rosàs <francescrosasbosque@gmail.com>
2011-12-12 19:39:49 +0100

    Pinged resource description

--- scripts/main.js
+++ scripts/main.js
@@ -15,7 +15,13 @@ require(['ping-url', 'page-title'], function(pingUrl, pageTitle) {
     })()
 
     ;(function continuousPing() {
+
+        // Resource has to be
+        // - Small
+        // - In a CDN (close to the user)
+        // - A Javascript (TODO really?)
         var url = 'http://ajax.googleapis.com/ajax/libs/chrome-frame/1.0.2/CFInstall.min.js'
+
         pingUrl(url, function(error, ping) {
             if (error) {
                 console.error(error)
