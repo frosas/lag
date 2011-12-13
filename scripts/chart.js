86bd17a033b064185229fc05a4f338530ff467cc
Francesc Rosàs <francescrosasbosque@gmail.com>
2011-12-13 23:06:44 +0100

    Use SVG

--- scripts/chart.js
+++ scripts/chart.js
@@ -2,25 +2,43 @@ define(function() {
 
     var pings = (function() {
         var pings = []
-        var max = 40
+        var max = 80
         return {
             add: function(ping) {
                 pings.push(ping)
                 pings = pings.slice(-max)
             },
-            pings: function() {
+            all: function() {
                 return pings
             }
         }
     })()
 
+    var element = d3.select('#chart').append('svg:svg')
+    var elementHeight = parseInt(element.style('height'))
+
+    var maxPing = 5000 // TODO DRY
+
+    var y = d3.scale.linear()
+        .domain([0, maxPing])
+        .range([0, elementHeight])
+
+    var barWidth = 10 
+    var setX = function(ping, i) { return i * barWidth }
+
     var update = function() {
-        var update = d3.select('#chart').selectAll('div')
-            .data(pings.pings(), function(ping) { return ping.start })
-        update.enter().append('div')
-            .style('height', function(ping) { return ping.lag + 'px' })
-            .append('div')
-                .text(function(ping) { return ping.lag })
+
+        var update = element.selectAll('rect')
+            .data(pings.all(), function(ping) { return ping.start })
+
+        update.enter().append('svg:rect')
+            .attr('x', setX)
+            .attr('y', function(ping) { return y(maxPing - ping.lag) })
+            .attr('width', barWidth)
+            .attr('height', function(ping) { return y(ping.lag)  })
+
+        update.attr('x', setX)
+
         update.exit().remove()
     }
 
