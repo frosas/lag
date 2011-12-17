a9c72575642d605360563dde1f2da8929e339aa4
Francesc Rosàs <francescrosasbosque@gmail.com>
2011-12-17 13:48:14 +0100

    Use more icons to indicate ping quality

--- public/scripts/page-icon.js
+++ public/scripts/page-icon.js
@@ -2,8 +2,10 @@ define(function() {
 
     var pingQualityIconUrl = function(ping) {
         if (ping.lag < 100) return 'images/bullet_green.png'
-        if (ping.lag < 200) return 'images/bullet_orange.png'
-        return 'images/bullet_red.png'
+        if (ping.lag < 200) return 'images/bullet_yellow.png'
+        if (ping.lag < 300) return 'images/bullet_orange.png'
+        if (ping.lag < 5000 /* TODO DRY */) return 'images/bullet_red.png'
+        return 'images/bullet_black.png'
     }
 
     var element = $('link[rel~=icon]')
