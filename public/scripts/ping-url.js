82442726fe2222af27455fbe89162a173199fa24
Francesc Rosàs <francescrosasbosque@gmail.com>
2011-12-19 17:50:33 +0100

    Removed more unnecessary parameters

--- public/scripts/ping-url.js
+++ public/scripts/ping-url.js
@@ -4,14 +4,13 @@ define(function() {
      * @param url It has to be a Javascript file
      */
     return function(url, callback) {
-        var start = new Date
         $.ajax({
             url: url,
             timeout: 5000,
             dataType: 'script',
             complete: function(xhr, status) {
                 var error = status in ['error', 'timeout'] ? status : null
-                callback(error, start)
+                callback(error)
             }
         })
     }
