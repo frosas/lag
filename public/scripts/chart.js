f6a36e819a469f0ebf6085d2cbaa458833a20c89
Francesc Rosàs <francescrosasbosque@gmail.com>
2012-10-06 16:45:28 +0200

    Reduce work inside the animation loop

--- public/scripts/chart.js
+++ public/scripts/chart.js
@@ -12,12 +12,12 @@ define(['chart-pings', 'common'], function(pings, common) {
     window.addEventListener('resize', onResize)
     onResize()
 
-    var yScale = function(ping) { 
-        // 0 -> 0, normalPing -> .1, ∞ -> 1
-        var normalPing = 200
-        var normalizedPing = Math.atan(ping / normalPing / 10) * 2 / Math.PI
+    var yScale = function(lag) { 
+        // 0 -> 0, normalLag -> .1, ∞ -> 1
+        var normalLag = 200
+        var normalizedLag = Math.atan(lag / normalLag / 10) * 2 / Math.PI
 
-        return normalizedPing * elementHeight 
+        return normalizedLag * elementHeight 
     }
 
     var selections = (function() {
@@ -27,8 +27,11 @@ define(['chart-pings', 'common'], function(pings, common) {
             updated: function() { return updated },
             exited: function() { return exited },
             update: function() {
+                var data = pings.all().map(function(ping) {
+                    return {ping: ping, exiting: false, ended: false}
+                })
                 updated = element.selectAll('rect')
-                    .data(pings.all(), function(ping) { return ping.start() })
+                    .data(data, function(datum) { return datum.ping.start() })
                 entered = updated.enter()
                 exited = updated.exit()
             }
@@ -39,14 +42,18 @@ define(['chart-pings', 'common'], function(pings, common) {
 
     d3.timer(function() {
         ;[selections.updated(), selections.exited()].forEach(function(selection) {
-            selection
-                .attr('y', function(ping) { return elementHeight - yScale(ping.lag()) })
-                .attr('height', function(ping) { return yScale(ping.lag()) })
-                .style('fill-opacity', function(ping) { return ping.end() ? 1 : 0.7 })
+            selection.each(function(datum) {
+                if (datum.ended) return
+                if (datum.ping.end()) datum.ended = true
+                d3.select(this)
+                    .attr('y', function() { return elementHeight - yScale(datum.ping.lag()) })
+                    .attr('height', function() { return yScale(datum.ping.lag()) })
+                    .style('fill-opacity', function() { return datum.ping.end() ? 1 : 0.7 })
+            })
 
             var now = Date.now()
             xScale.domain([now - pings.max() * common.pingInterval, now])
-            selection.attr('x', function(ping) { return xScale(ping.start()) })
+            selection.attr('x', function(datum) { return xScale(datum.ping.start()) })
         })
     })
 
@@ -58,10 +65,10 @@ define(['chart-pings', 'common'], function(pings, common) {
 
             selections.entered().append('svg:rect').attr('width', common.barWidth)
 
-            selections.exited().each(function(ping) {
-                if (this.getAttribute('data-exiting')) return
+            selections.exited().each(function(datum) {
+                if (datum.exiting) return
+                datum.exiting = true
                 d3.select(this).transition().duration(common.pingInterval * 2).remove()
-                this.setAttribute('data-exiting', true)
             })
 
             return ping
