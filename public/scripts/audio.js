0986b9cb40bc40b0c2f244d4966a330082471bd3
Francesc Rosàs <francescrosasbosque@gmail.com>
2012-10-11 08:51:21 +0200

    Almost no sound under normal ping

--- public/scripts/audio.js
+++ public/scripts/audio.js
@@ -2,14 +2,14 @@ define(['audiolet'], function() {
     return function(pings) {
         var audiolet = new Audiolet
         var sine = new WhiteNoise(audiolet)
-        var filter = new LowPassFilter(audiolet, 200)
+        var filter = new LowPassFilter(audiolet, 1)
         sine.connect(filter)
         filter.connect(audiolet.output)
         setInterval(function() { 
             // TODO More easing (now one can hear frequency changing steps)
             // TODO More "windy" (add light random frequency variations trough every few seconds)
             var currentFrequency = filter.frequency.getValue()
-            var targetFrequency = 50 + pings.currentLag() / 4
+            var targetFrequency = pings.currentLag() / 4
             var nextFrequency = currentFrequency + (targetFrequency - currentFrequency) / 8
             filter.frequency.setValue(nextFrequency) 
         }, 100)
