346450b7408ed1df2fb23a33cc2bfb1626d50683
Francesc Rosàs <francescrosasbosque@gmail.com>
2012-11-26 12:59:08 +0100

    Alert about require.js errors

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -11,6 +11,16 @@ require.config({
     }
 })
 
+requirejs.onError = function(error) {
+    if (error.requireType == 'timeout') {
+        alert("Timeout reached loading the dependencies. Please, try reloading the page.")
+    } else {
+        alert("Unknown error found loading the dependencies. See console for details.")
+    }
+
+    throw error
+}
+
 require(['chart', 'page-title', 'page-icon', 'pings', 'title', 'user', 'audio', 'controls'], function(Chart, PageTitle, PageIcon, Pings, Title, User, Audio, Controls) {
     var user = new User
     var pings = new Pings
