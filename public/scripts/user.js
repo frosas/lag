23f52b7c3b9545c8ace72765af9e56672e10675d
Francesc Rosàs <francescrosasbosque@gmail.com>
2012-12-24 17:04:18 +0100

    Fix comment

--- public/scripts/user.js
+++ public/scripts/user.js
@@ -2,7 +2,7 @@ define(['realtime-set-interval', 'underscore', 'backbone', 'd3'], function(realt
     return function() {
         var user = {}
         _.extend(user, Backbone.Events)
-        // User won't notice higher intervals than these
+        // User won't notice lower intervals than these
         setInterval(function() { user.trigger('read') }, 250)
         d3.timer(function() { user.trigger('view') })
         realtimeSetInterval(function() { user.trigger('hear') }, 100)
