ce75178f44042f5a230e123808ea4fe59b952200
Francesc Rosàs <francescrosasbosque@gmail.com>
2012-12-24 16:45:44 +0100

    Upgrade to D3 v3

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -3,7 +3,7 @@ require.config({
         underscore: 'http://documentcloud.github.com/underscore/underscore-min',
         backbone: 'http://backbonejs.org/backbone-min',
         jquery: 'http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min',
-        d3: 'http://d3js.org/d3.v2.min',
+        d3: 'http://d3js.org/d3.v3.min',
         audiolet: 'https://raw.github.com/oampo/Audiolet/master/src/audiolet/Audiolet.min'
     },
     shim: {
