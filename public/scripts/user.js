303a4419ab3b618b7c524e86dcc8cc377392e39d
Francesc Rosàs <francescrosasbosque@gmail.com>
2012-12-28 14:14:54 +0100

    Use requestAnimationFrame() directly

--- public/scripts/user.js
+++ public/scripts/user.js
@@ -1,11 +1,28 @@
 define(['realtime-set-interval', 'underscore', 'backbone', 'd3'], function(realtimeSetInterval) {
+    // From http://paulirish.com/2011/requestanimationframe-for-smart-animating/
+    var requestAnimationFrame = 
+        window.requestAnimationFrame || 
+        window.webkitRequestAnimationFrame || 
+        window.mozRequestAnimationFrame || 
+        window.oRequestAnimationFrame || 
+        window.msRequestAnimationFrame || 
+        function(callback) { setTimeout(callback, 1000 / 60) }
+
     return function() {
-        var user = {}
-        _.extend(user, Backbone.Events)
+        var user = _.extend({}, Backbone.Events)
+
         // User won't notice lower intervals than these
+
         setInterval(function() { user.trigger('read') }, 250)
-        d3.timer(function() { user.trigger('view') })
+
+        // Consumes less CPU than d3.timer() (2.74% vs 7.63%)
+        ;(function userViewTimer() {
+            requestAnimationFrame(userViewTimer)
+            user.trigger('view')
+        })()
+
         realtimeSetInterval(function() { user.trigger('hear') }, 100)
+
         return user
     }
 })
