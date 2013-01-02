2f95319c92f8843eed5b5b262e6acf40fa277a15
Francesc Rosàs <francescrosasbosque@gmail.com>
2013-01-02 21:57:17 +0100

    Ping creating DOM elements directly

--- public/scripts/ping.js
+++ public/scripts/ping.js
@@ -1,34 +1,25 @@
 define(['underscore', 'backbone'], function() {
     return function() {
-        var start, end, request
+        var start, end
 
         var ping = {
-            lag: function() {
-                return (end || Date.now()) - start
-            },
-            start: function() {
-                return start
-            },
-            end: function() {
-                return end
-            }
+            lag: function() { return (end || Date.now()) - start },
+            start: function() { return start },
+            end: function() { return end }
         }
 
         _.extend(ping, Backbone.Events)
 
-        $.ajax({
-            // Resource shall be small, close to the user (eg, in a CDN) and in the web (not in localhost or the
-            // intranet)
-            url: 'http://lag.frosas.net/scripts/blank.js',
-            timeout: 999999999, // "Forever"
-            dataType: 'script',
-            beforeSend: function() { start = Date.now() },
-            success: function() { 
-                end = Date.now()
-                ping.trigger('pong')
-            },
-            error: function(xhr, status, error) { console.error(error) }
+        var script = document.createElement('script')
+        script.async = true
+        script.addEventListener('load', function() {
+            end = Date.now()
+            script.parentNode.removeChild(script)
+            ping.trigger('pong')
         })
+        start = Date.now()
+        script.src = 'http://lag.frosas.net/scripts/blank.js?' + start
+        document.head.appendChild(script)
 
         return ping
     }
