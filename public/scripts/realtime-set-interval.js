218c55a3bd6404865136b6248f92f8e6332a42d6
Francesc Rosàs <francescrosasbosque@gmail.com>
2013-02-18 11:45:18 +0100

    Revert "Use BlobBuilder (supported by Chrome for Android)"
    This reverts commit 844d4d4410b9fd73cb49a01ab1c975c3bb84d017.

--- public/scripts/realtime-set-interval.js
+++ public/scripts/realtime-set-interval.js
@@ -1,14 +1,11 @@
 // Based on http://www.html5rocks.com/en/tutorials/workers/basics/
 define(function() {
     var URL = window.URL || window.webkitURL
-    // BlobBuilder is deprecated but Chrome for Android fails with an "Illegal constructor" 
-    // instantiating the Blob directly
-    var BlobBuilder = window.BlobBuilder || window.WebKitBlobBuilder
-    if (URL && Worker && Blob && BlobBuilder) {
-        var blobBuilder = new BlobBuilder
-        blobBuilder.append('self.onmessage = function(event) { setInterval(function() { self.postMessage({}) }, event.data.interval) }')
-        var blob = blobBuilder.getBlob('text/javascript')
-        var blobUrl = URL.createObjectURL(blob)
+    if (URL && Worker && Blob) {
+        var blobUrl = URL.createObjectURL(new Blob(
+            ['self.onmessage = function(event) { setInterval(function() { self.postMessage({}) }, event.data.interval) }'],
+            {type: 'text/javascript'} 
+        ))
         return function(callback, interval) {
             var worker = new Worker(blobUrl)
             worker.addEventListener('message', callback)
