a7319e81fcefbcb0d3128129fee25441e2fe8834
Francesc Rosàs <francescrosasbosque@gmail.com>
2013-03-05 16:09:39 +0100

    Test on the browser

--- test/math.js
+++ test/math.js
@@ -1,45 +1,35 @@
-var requirejs = require('requirejs')
-require('chai').should()
+define(['public/scripts/math'], function(_Math) {
+    describe('Math', function() {
+        describe('equal()', function() {
+            it('should be true with same integer', function() {
+                _Math.equal(1, 1).should.equal(true)
+            })
 
-describe('Math', function() {
-    var Math
+            it('should be false with distinct integers', function() {
+                _Math.equal(1, 2).should.equal(false)
+            })
 
-    before(function(done) {
-        requirejs(['public/scripts/math.js'], function(_Math) {
-            Math = _Math
-            done()
-        })
-    })
+            it('should be true with similar floats', function() {
+                _Math.equal(1.01, 1.02, 1).should.equal(true)
+            })
 
-    describe('equal()', function() {
-        it('should be true with same integer', function() {
-            Math.equal(1, 1).should.equal(true)
-        })
+            it('should be false with slightly distinct floats', function() {
+                _Math.equal(1, 1.1, 1).should.equal(false)
+            })
 
-        it('should be false with distinct integers', function() {
-            Math.equal(1, 2).should.equal(false)
-        })
-
-        it('should be true with similar floats', function() {
-            Math.equal(1.01, 1.02, 1).should.equal(true)
+            it('should truncate, not round', function() {
+                _Math.equal(1, 1.9).should.equal(true)
+            })
         })
 
-        it('should be false with slightly distinct floats', function() {
-            Math.equal(1, 1.1, 1).should.equal(false)
-        })
+        describe('polarity()', function() {
+            it('should be 1 with a positive number', function() {
+                _Math.polarity(5).should.equal(1)
+            })
 
-        it('should truncate, not round', function() {
-            Math.equal(1, 1.9).should.equal(true)
-        })
-    })
-
-    describe('polarity()', function() {
-        it('should be 1 with a positive number', function() {
-            Math.polarity(5).should.equal(1)
-        })
-
-        it('should be -1 with a negative number', function() {
-            Math.polarity(-5).should.equal(-1)
+            it('should be -1 with a negative number', function() {
+                _Math.polarity(-5).should.equal(-1)
+            })
         })
     })
 })
