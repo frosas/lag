8d03fd36fbc1fb86f701d944be1a8d20a068d8da
Francesc Rosàs <francescrosasbosque@gmail.com>
2013-06-10 08:26:50 +0100

    Ping a CDN

--- public/scripts/ping.js
+++ public/scripts/ping.js
@@ -11,11 +11,15 @@ define(['underscore', 'backbone'], function() {
     }
 
     Ping.prototype.load = function() {
+        // Here we have a copy of the site in a CDN close to the user. Ensure the 
+        // CDN is configured to not forward query strings to reduce latency.
+        var url = 'http://d18ks85av1x0pi.cloudfront.net/scripts/blank.js'
+
         this.script = document.createElement('script')
         this.script.async = true
         this.script.addEventListener('load', _.bind(this.onLoad, this))
         this.start = Date.now()
-        this.script.src = 'http://lag.frosas.net/scripts/blank.js?' + this.start
+        this.script.src = url + '?' + this.start
         document.head.appendChild(this.script)
     }
 
