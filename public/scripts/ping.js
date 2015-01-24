334916ba9a6570bb069517dd0dc27c8d9560ad90
Francesc Rosàs <francescrosasbosque@gmail.com>
2015-01-24 11:17:35 +0000

    Make internal properties "private"

--- public/scripts/ping.js
+++ public/scripts/ping.js
@@ -3,36 +3,30 @@
 define(['underscore', 'backbone'], function(_, Backbone) {
     var Ping = function() {
         _.extend(this, Backbone.Events);
-        this.load();
+        this._send();
     };
-
-    Ping.prototype.end = null;
 
     Ping.prototype.lag = function() { 
         return (this.end || Date.now()) - this.start;
     };
-
-    Ping.prototype.load = function() {
+    
+    Ping.prototype._send = function() {
         // Here we have a copy of the site in a CDN close to the user. Ensure the 
         // CDN is configured to not forward query strings to reduce latency.
         var url = 'http://d18ks85av1x0pi.cloudfront.net/scripts/blank.js';
 
-        this.script = document.createElement('script');
-        this.script.async = true;
-        this.script.addEventListener('load', this.onLoad.bind(this));
+        this._script = document.createElement('script');
+        this._script.async = true;
+        this._script.addEventListener('load', this._onLoad.bind(this));
         this.start = Date.now();
-        this.script.src = url + '?' + this.start;
-        document.head.appendChild(this.script);
+        this._script.src = url + '?' + this.start;
+        document.head.appendChild(this._script);
     };
 
-    Ping.prototype.onLoad = function() {
+    Ping.prototype._onLoad = function() {
         this.end = Date.now();
         this.trigger('pong');
-        this.destroy();
-    };
-    
-    Ping.prototype.destroy = function () {
-        this.script.parentNode.removeChild(this.script);
+        this._script.parentNode.removeChild(this._script);
     };
 
     return Ping;
