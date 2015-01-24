e841910af9307830a86fea80c6d44722a6a553ea
Francesc Rosàs <francescrosasbosque@gmail.com>
2015-01-24 13:36:40 +0000

    Lint main.js

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,17 +1,20 @@
-var debug = true
+/* eslint-env amd */
+/* global console, alert */
+
+var debug = true;
 
 // Error handling
 
 onerror = function(message, file, line, column, error) { 
-    if (error) console.error('Uncaught error', error)
-    alert(error && error.message || error || message) 
-}
+    if (error) console.error('Uncaught error', error);
+    alert(error && error.message || error || message);
+};
 
 require.onError = function(error) { 
     // Call onerror() directly as throwing the error makes the browser show a 
     // "Script error" error
-    onerror('', '', '', '', error) 
-}
+    onerror('', '', '', '', error);
+};
 
 //
 
@@ -26,18 +29,18 @@ require.config({
         'backbone': ['underscore']
     },
     waitSeconds: 5 /* mins */ * 60
-})
+});
 
-require(['chart', 'page-title', 'page-icon', 'pings', 'title', 'user', 'audio', 
-    'controls'], function(Chart, PageTitle, PageIcon, Pings, Title, User, Audio, 
-    Controls) {
-        
-    var user = new User
-    var pings = new Pings
-    new Chart(user, pings)
-    new PageTitle(user, pings)
-    new PageIcon(user, pings)
-    new Title(user, pings)
-    var audio = Audio.create(user, pings)
-    new Controls(audio)
-})
+require(
+    ['chart', 'page-title', 'page-icon', 'pings', 'title', 'user', 'audio', 'controls'], 
+    function(Chart, PageTitle, PageIcon, Pings, Title, User, Audio, Controls) {            
+        var user = new User;
+        var pings = new Pings;
+        new Chart(user, pings);
+        new PageTitle(user, pings);
+        new PageIcon(user, pings);
+        new Title(user, pings);
+        var audio = Audio.create(user, pings);
+        new Controls(audio);
+    }
+);
