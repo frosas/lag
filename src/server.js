4f3d1f9c601b82a0ca4d806101aba23e65c73ef7
Francesc Rosàs <francescrosasbosque@gmail.com>
2015-01-30 00:16:17 +0000

    Simplify server

--- src/server.js
+++ src/server.js
@@ -2,10 +2,5 @@
 
 var express = require('express');
 var app = express();
-
-app.set('port', process.env.PORT || 5000);
-app.use(express.static(__dirname + '/../public'));
-
-app.listen(app.get('port'), function() {
-    console.log("Server running at http://localhost:" + app.get('port'));
-});
+app.use(express.static(__dirname + '/../public')); 
+app.listen(process.env.PORT || 5000);
