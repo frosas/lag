02fda4b0ccb92967f3b0bb17d6fa78124fccfb0e
Francesc Rosàs <francescrosasbosque@gmail.com>
2015-08-22 17:20:39 +0100

    Cache static resources for a year

--- src/server.js
+++ src/server.js
@@ -5,5 +5,7 @@ var compression = require('compression');
 
 var app = express();
 app.use(compression());
-app.use(express.static(__dirname + '/../public')); 
+app.use(express.static(__dirname + '/../public', { // eslint-disable-line
+    maxAge: 365 /* days */ * 24 * 60 * 60 * 1000
+}));
 app.listen(process.env.PORT || 5000);
