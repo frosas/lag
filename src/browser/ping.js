649f5ada8b98f05a6d2ea14268e6659e99411919
Francesc Rosàs <francescrosasbosque@gmail.com>
2015-08-22 21:12:47 +0100

    Ping from prototype to class

--- src/browser/ping.js
+++ src/browser/ping.js
@@ -1,4 +1,4 @@
-/* eslint-env node */
+/* eslint-env node, es6 */
 
 var _ = require('underscore');
 var Backbone = require('backbone');
@@ -7,53 +7,43 @@ var $ = require('jquery');
 // Here we have a copy of the site in a CDN close to the user. Ensure the 
 // CDN is configured to not forward query strings to reduce latency.
 var URL_ = 'http://d18ks85av1x0pi.cloudfront.net/scripts/blank.js';
-    
-var Ping = module.exports = function() {
-    _.extend(this, Backbone.Events);
-    this._send();
-};
 
-/**
- * Whether it has finished (whether succesfully or not)
- * 
- * @type {boolean}
- */
-Ping.prototype.done = false;
+module.exports = class Ping {
+    constructor() {
+        _.extend(this, Backbone.Events);
+        this.done = false; // Whether it has finished (whether succesfully or not)
+        this.failed = false; // Whether it finished failing
+        this._send();
+    }
 
-/**
- * Whether it finished failing
- * 
- * @type {boolean}
- */
-Ping.prototype.failed = false;
+    lag() { 
+        return (this.end || Date.now()) - this.start;
+    };
 
-Ping.prototype.lag = function() { 
-    return (this.end || Date.now()) - this.start;
-};
+    /**
+     * Cancels the ping
+     */
+    abort() {
+        this._request.abort();
+        this.done = true;
+        this.failed = true;
+    };
 
-/**
- * Cancels the ping
- */
-Ping.prototype.abort = function () {
-    this._request.abort();
-    this.done = true;
-    this.failed = true;
-};
+    toString() {
+        return 'Ping started at ' + new Date(this.start);
+    };
 
-Ping.prototype.toString = function () {
-    return 'Ping started at ' + new Date(this.start);
-};
+    _send() {
+        var ping = this;
+        this.start = Date.now();
+        this._request = $.ajax({url: URL_, dataType: 'script'});
+        this._request.then(this._onPong.bind(this));
+        this._request.then(null, () => ping.abort());
+    };
 
-Ping.prototype._send = function() {
-    var ping = this;
-    this.start = Date.now();
-    this._request = $.ajax({url: URL_, dataType: 'script'});
-    this._request.then(this._onPong.bind(this));
-    this._request.then(null, function () { ping.abort(); });
-};
-
-Ping.prototype._onPong = function() {
-    this.done = true;
-    this.end = Date.now();
-    this.trigger('pong');
-};
+    _onPong() {
+        this.done = true;
+        this.end = Date.now();
+        this.trigger('pong');
+    };
+}
--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,10 @@ module.exports = {
     output: {
         path: './public/compiled/scripts',
         filename: 'main.js'
+    },
+    module: {
+        loaders: [
+            {test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'}
+        ]
     }
 };
