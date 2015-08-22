8c170fead76e7b62a99c6ab1b1671f2bbc3dac84
Francesc Rosàs <francescrosasbosque@gmail.com>
2015-08-22 20:42:10 +0100

    Ping from AMD to CommonJS

--- src/browser/ping.js
+++ src/browser/ping.js
@@ -1,60 +1,60 @@
-/* eslint-env amd */
+/* eslint-env node */
 
-define(['underscore', 'backbone', 'jquery'], function(_, Backbone, $) {
-    var Ping = function() {
-        _.extend(this, Backbone.Events);
-        this._send();
-    };
-    
-    /**
-     * Whether it has finished (whether succesfully or not)
-     * 
-     * @type {boolean}
-     */
-    Ping.prototype.done = false;
-    
-    /**
-     * Whether it finished failing
-     * 
-     * @type {boolean}
-     */
-    Ping.prototype.failed = false;
+var _ = require('underscore');
+var Backbone = require('backbone');
+var $ = require('jquery');
 
-    Ping.prototype.lag = function() { 
-        return (this.end || Date.now()) - this.start;
-    };
-    
-    /**
-     * Cancels the ping
-     */
-    Ping.prototype.abort = function () {
-        this._request.abort();
-        this.done = true;
-        this.failed = true;
-    };
-    
-    Ping.prototype.toString = function () {
-        return 'Ping started at ' + new Date(this.start);
-    };
-    
-    Ping.prototype._send = function() {
-        var ping = this;
-        
-        // Here we have a copy of the site in a CDN close to the user. Ensure the 
-        // CDN is configured to not forward query strings to reduce latency.
-        var url = 'http://d18ks85av1x0pi.cloudfront.net/scripts/blank.js';
-        
-        this.start = Date.now();
-        this._request = $.ajax({url: url, dataType: 'script'});
-        this._request.then(this._onLoad.bind(this));
-        this._request.then(null, function () { ping.abort(); });
-    };
+var Ping = module.exports = function() {
+    _.extend(this, Backbone.Events);
+    this._send();
+};
 
-    Ping.prototype._onLoad = function() {
-        this.done = true;
-        this.end = Date.now();
-        this.trigger('pong');
-    };
+/**
+ * Whether it has finished (whether succesfully or not)
+ * 
+ * @type {boolean}
+ */
+Ping.prototype.done = false;
 
-    return Ping;
-});
+/**
+ * Whether it finished failing
+ * 
+ * @type {boolean}
+ */
+Ping.prototype.failed = false;
+
+Ping.prototype.lag = function() { 
+    return (this.end || Date.now()) - this.start;
+};
+
+/**
+ * Cancels the ping
+ */
+Ping.prototype.abort = function () {
+    this._request.abort();
+    this.done = true;
+    this.failed = true;
+};
+
+Ping.prototype.toString = function () {
+    return 'Ping started at ' + new Date(this.start);
+};
+
+Ping.prototype._send = function() {
+    var ping = this;
+    
+    // Here we have a copy of the site in a CDN close to the user. Ensure the 
+    // CDN is configured to not forward query strings to reduce latency.
+    var url = 'http://d18ks85av1x0pi.cloudfront.net/scripts/blank.js';
+    
+    this.start = Date.now();
+    this._request = $.ajax({url: url, dataType: 'script'});
+    this._request.then(this._onLoad.bind(this));
+    this._request.then(null, function () { ping.abort(); });
+};
+
+Ping.prototype._onLoad = function() {
+    this.done = true;
+    this.end = Date.now();
+    this.trigger('pong');
+};
