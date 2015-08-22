9e3af5b3a6a6f4c93bec3061279ca987f801637b
Francesc Rosàs <francescrosasbosque@gmail.com>
2015-08-22 15:50:21 +0100

    Compress resources

--- src/server.js
+++ src/server.js
@@ -1,6 +1,9 @@
 /* eslint-env node */
 
 var express = require('express');
+var compression = require('compression');
+
 var app = express();
+app.use(compression());
 app.use(express.static(__dirname + '/../public')); 
 app.listen(process.env.PORT || 5000);
