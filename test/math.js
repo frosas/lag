0151bd26af118a14764cb371a122a992f9cf3cf4
Francesc Rosàs <francescrosasbosque@gmail.com>
2017-01-12 11:09:25 +0000

    Chai to don't pollute objects

--- test/math.js
+++ test/math.js
@@ -1,17 +1,16 @@
 /* eslint-env mocha */
 
 const math = require('../src/browser/math');
-
-require('chai').should();
+const expect = require('chai').expect;
 
 describe('math', () => {
   describe('polarity()', () => {
     it('should be 1 with a positive number', () => {
-      math.polarity(5).should.equal(1);
+      expect(math.polarity(5)).to.equal(1);
     });
 
     it('should be -1 with a negative number', () => {
-      math.polarity(-5).should.equal(-1);
+      expect(math.polarity(-5)).to.equal(-1);
     });
   });
 });
