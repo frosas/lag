8e37f4b1ceb666da65ac053e6e72d9bc11949a5e
Francesc Rosàs <francescrosasbosque@gmail.com>
2017-01-12 11:06:29 +0000

    Remove math.equal()
    It's not used

--- src/browser/math.js
+++ src/browser/math.js
@@ -1,10 +1,3 @@
 module.exports = {
-  equal: (a, b, decimals) => {
-    if (decimals == null) decimals = 0;
-    a *= Math.pow(10, decimals); // eslint-disable-line no-restricted-properties
-    b *= Math.pow(10, decimals); // eslint-disable-line no-restricted-properties
-    return !(parseInt(a, 10) - parseInt(b, 10));
-  },
-
   polarity: number => (number >= 0 ? 1 : -1),
 };
--- test/math.js
+++ test/math.js
@@ -4,29 +4,7 @@ const math = require('../src/browser/math');
 
 require('chai').should();
 
-describe('Math', () => {
-  describe('equal()', () => {
-    it('should be true with same integer', () => {
-      math.equal(1, 1).should.equal(true);
-    });
-
-    it('should be false with distinct integers', () => {
-      math.equal(1, 2).should.equal(false);
-    });
-
-    it('should be true with similar floats', () => {
-      math.equal(1.01, 1.02, 1).should.equal(true);
-    });
-
-    it('should be false with slightly distinct floats', () => {
-      math.equal(1, 1.1, 1).should.equal(false);
-    });
-
-    it('should truncate, not round', () => {
-      math.equal(1, 1.9).should.equal(true);
-    });
-  });
-
+describe('math', () => {
   describe('polarity()', () => {
     it('should be 1 with a positive number', () => {
       math.polarity(5).should.equal(1);
