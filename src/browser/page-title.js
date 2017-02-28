94491a2ac736637c92ccee8c758551bf78a49d79
Francesc Rosàs <francescrosasbosque@gmail.com>
2017-02-28 22:34:20 +0000

    Increase the chances of seeing the actual lag when on narrow page tabs

--- src/browser/page-title.js
+++ src/browser/page-title.js
@@ -3,6 +3,6 @@ const lag = require('./lag');
 module.exports = (user, pings) => {
   const original = document.title;
   user.events.on('read', () => {
-    document.title = `${original} (${lag.humanize(pings.currentLag())})`;
+    document.title = `${lag.humanize(pings.currentLag())} - ${original}`;
   });
 };
