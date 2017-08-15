685451ba1653836d3e8c7effa646d366bb98dc9c
Francesc Rosas <francescrosasbosque@gmail.com>
2017-08-15 12:16:44 +0200

    Apply Inferno and JSX ESLint config to browser code only

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,13 @@
-// See http://eslint.org/docs/
 module.exports = {
-    env: {es6: true, commonjs: true},
-    parserOptions: {
-        ecmaVersion: 2017,
-        ecmaFeatures: {jsx: true}
-    },
-    extends: ['eslint:recommended', 'plugin:inferno/recommended'],
-    plugins: ['prettier', 'inferno'],
-    rules: {
-        'prettier/prettier': ['error', {
-            singleQuote: true, 
-            bracketSpacing: false,
-            trailingComma: 'es5'
-        }],
-        'inferno/jsx-uses-inferno': 'error',
-        'inferno/jsx-uses-vars': 'error'
-    }
+  env: {es6: true, commonjs: true},
+  parserOptions: {ecmaVersion: 2017},
+  extends: ['eslint:recommended'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': ['error', {
+      singleQuote: true, 
+      bracketSpacing: false,
+      trailingComma: 'es5'
+    }]    
+  }
 }
--- src/browser/.eslintrc.js
+++ src/browser/.eslintrc.js
@@ -1,4 +1,13 @@
 module.exports = {
-  env: { browser: true },
-  parserOptions: { sourceType: 'module' }
+  env: {browser: true},
+  parserOptions: {
+    sourceType: 'module',
+    ecmaFeatures: {jsx: true}
+  },
+  extends: ['plugin:inferno/recommended'],
+  plugins: ['inferno'],
+  rules: {
+    'inferno/jsx-uses-inferno': 'error',
+    'inferno/jsx-uses-vars': 'error'
+  }
 };
