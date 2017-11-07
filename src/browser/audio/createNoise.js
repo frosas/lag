ba61797226344cd672b3c70a1e3aa1724f6b354d
Francesc Rosas <francescrosasbosque@gmail.com>
2017-11-07 09:40:55 +0100

    Extract createNoiseBuffer()

--- src/browser/audio/createNoise.js
+++ src/browser/audio/createNoise.js
@@ -1,4 +1,12 @@
 export default audioContext => {
+  const source = audioContext.createBufferSource();
+  source.buffer = createNoiseBuffer(audioContext);
+  source.loop = true;
+  source.start(0);
+  return source;
+};
+
+const createNoiseBuffer = audioContext => {
   const lengthInSeconds = 5;
   const buffer = audioContext.createBuffer(
     1,
@@ -7,10 +15,5 @@ export default audioContext => {
   );
   const data = buffer.getChannelData(0);
   for (let i = data.length; i; i -= 1) data[i] = Math.random() * 2 - 1;
-
-  const source = audioContext.createBufferSource();
-  source.buffer = buffer;
-  source.loop = true;
-  source.start(0);
-  return source;
+  return buffer;
 };
