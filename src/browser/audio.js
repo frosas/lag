84cda2613f96ad3f8cf9b479e9f6e4b9f39c935f
Francesc Rosas <francescrosasbosque@gmail.com>
2017-11-07 09:33:27 +0100

    Extract audio/Noise

--- src/browser/audio.js
+++ src/browser/audio.js
@@ -1,21 +1,5 @@
 import math from "../universal/math";
-
-function Noise(context) {
-  const lengthInSeconds = 5;
-  const buffer = context.createBuffer(
-    1,
-    context.sampleRate * lengthInSeconds,
-    context.sampleRate
-  );
-  const data = buffer.getChannelData(0);
-  for (let i = data.length; i; i -= 1) data[i] = Math.random() * 2 - 1;
-
-  const source = context.createBufferSource();
-  source.buffer = buffer;
-  source.loop = true;
-  source.start(0);
-  return source;
-}
+import Noise from "./audio/Noise";
 
 export default class Audio {
   constructor(user, pings) {
