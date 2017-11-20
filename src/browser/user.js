283cce04ac36a01e2833d1396d2471c8c417f9b7
Francesc Rosas <francescrosasbosque@gmail.com>
2017-11-20 18:16:19 +0100

    User prototype -> class

--- src/browser/user.js
+++ src/browser/user.js
@@ -3,31 +3,30 @@ import Events from "events";
 // User won't notice lower intervals than these
 const MAX_READ_INTERVAL = 250; // msecs
 
-export default () => {
-  const user = { events: new Events() };
+export default class {
+  constructor() {
+    this.events = new Events();
 
-  const triggerReadIfNeeded = (() => {
-    let lastRead;
-    return () => {
-      const now = Date.now();
-      if (!lastRead || now > lastRead + MAX_READ_INTERVAL) {
-        user.events.emit("read");
-        lastRead = now;
-      }
-    };
-  })();
+    this._triggerViewEventPeriodically();
 
-  (function _userViewTimer() {
-    requestAnimationFrame(_userViewTimer);
-    user.events.emit("view");
-    triggerReadIfNeeded();
-  })();
+    // requestanimationframe() is not always triggered when the tab is not
+    // active. Here we ensure it is called often enough
+    setInterval(() => this._triggerReadEventIfNeeded(), MAX_READ_INTERVAL);
 
-  // requestanimationframe() is not always triggered when the tab is not
-  // active. Here we ensure it is called often enough
-  setInterval(triggerReadIfNeeded, MAX_READ_INTERVAL);
+    setInterval(() => this.events.emit("hear"), 250);
+  }
 
-  setInterval(() => user.events.emit("hear"), 250);
+  _triggerViewEventPeriodically() {
+    requestAnimationFrame(() => this._triggerViewEventPeriodically());
+    this.events.emit("view");
+    this._triggerReadEventIfNeeded();
+  }
 
-  return user;
-};
+  _triggerReadEventIfNeeded() {
+    const now = Date.now();
+    if (!this._lastRead || now > this._lastRead + MAX_READ_INTERVAL) {
+      this.events.emit("read");
+      this._lastRead = now;
+    }
+  }
+}
