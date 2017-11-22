f86c5f400667e1d5392cc2ff9c6287511e882133
Francesc Rosas <francescrosasbosque@gmail.com>
2017-11-22 17:14:09 +0100

    util.js -> .ts

---
+++ src/browser/util.ts
@@ -0,0 +1,12 @@
+export default {
+  humanizeLag: (amount: number): string => {
+    let fractionDigits = 0;
+    let unit = "ms";
+    if (amount > 1000) {
+      amount = amount / 1000;
+      fractionDigits = 1;
+      unit = "s";
+    }
+    return `${amount.toFixed(fractionDigits)} ${unit}`;
+  }
+};
