d9cae6365aa322789d4d2649bfebe8b56921cdc3
Francesc Rosas <francescrosasbosque@gmail.com>
2017-11-23 18:59:08 +0100

    assertType()

--- src/browser/util.ts
+++ src/browser/util.ts
@@ -8,3 +8,10 @@ export const humanizeLag = (amount: number): string => {
   }
   return `${amount.toFixed(fractionDigits)} ${unit}`;
 };
+
+// TODO Is there a way to achieve the same without using any function?
+export const assertType = <T>(value: any): T => {
+  // TODO Is there a way to mention the type (T) we were expecting?
+  if (value == null) throw new Error(`Expected a type but got ${value}`);
+  return value;
+};
