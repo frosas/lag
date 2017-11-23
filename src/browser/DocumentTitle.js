f3e18907a07fc8c361f16b021a21f2ff713dd932
Francesc Rosas <francescrosasbosque@gmail.com>
2017-11-23 23:36:21 +0100

    Fix DocumentTitle using util.humanizeLag()

--- src/browser/DocumentTitle.js
+++ src/browser/DocumentTitle.js
@@ -1,10 +1,10 @@
-import util from "./util";
+import { humanizeLag } from "./util";
 
 export default class {
   constructor(user, pings) {
     const original = document.title;
     user.events.on("read", () => {
-      document.title = `${util.humanizeLag(pings.currentLag)} - ${original}`;
+      document.title = `${humanizeLag(pings.currentLag)} - ${original}`;
     });
   }
 }
