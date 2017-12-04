75ca91cabee629c12bb40ec4d82c2949c41944ff
Francesc Rosas <francescrosasbosque@gmail.com>
2017-12-04 18:23:27 +0100

    Extract A_LONG_TIME

--- src/node/webServer.js
+++ src/node/webServer.js
@@ -2,14 +2,15 @@ const express = require("express");
 const compression = require("compression");
 const debug = require("debug")("app:http");
 
+const A_LONG_TIME = 1 /* year */ * 365 * 24 * 60 * 60;
+
 const app = express();
 
 app.use(compression());
 
 app.use((req, res, next) => {
   if (req.query.v) {
-    const aLongTime = 1 /* year */ * 365 * 24 * 60 * 60;
-    res.append("Cache-Control", `max-age=${aLongTime}, 1`);
+    res.append("Cache-Control", `max-age=${A_LONG_TIME}, 1`);
   }
   if (req.path == "/scripts/service-worker.js")
     res.append("Service-Worker-Allowed", "/");
