3b40ac92dd1c758f5bbd42d8310086c508c897ce
Francesc Rosas <francescrosasbosque@gmail.com>
2017-12-05 16:35:27 +0100

    Refacts

--- src/browser/chart/Component.js
+++ src/browser/chart/Component.js
@@ -9,10 +9,10 @@ export default class extends React.Component {
   }
 
   render() {
-    this.props.pings.max = this.props.width / this._barWidth + 1;
-    this._xScale.range([0, this.props.width]);
-    const pings = this.props.pings;
     const now = Date.now();
+    const pings = this.props.pings;
+    pings.max = this.props.width / this._barWidth + 1;
+    this._xScale.range([0, this.props.width]);
     this._xScale.domain([now - (pings.max - 1) * pings.interval, now]);
     return (
       <svg>
