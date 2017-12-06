18ac73b91a7667b4ea54afa1e3212bfa089cc1a2
Francesc Rosas <francescrosasbosque@gmail.com>
2017-12-06 01:58:56 +0100

    Refact

--- src/browser/Chart.tsx
+++ src/browser/Chart.tsx
@@ -5,17 +5,17 @@ import Pings from "./Pings";
 import User from "./User";
 
 export default class {
-  constructor(user: User, pings: Pings, chartEl: HTMLElement) {
+  constructor(user: User, pings: Pings, element: HTMLElement) {
     user.events.on("view", () => {
       ReactDOM.render(
         <ChartComponent
           pings={pings}
           // We use the parent node dimensions as Firefox doesn't seem to work
           // with the svg element ones.
-          width={chartEl.offsetWidth}
-          height={chartEl.offsetHeight}
+          width={element.offsetWidth}
+          height={element.offsetHeight}
         />,
-        chartEl
+        element
       );
     });
   }
