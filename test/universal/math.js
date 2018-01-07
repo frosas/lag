3ae34889e04faa27d707b2ecea7d7e47a09bef20
Francesc Rosas <francescrosasbosque@gmail.com>
2018-01-07 19:12:19 +0100

    Refact

--- test/universal/math.js
+++ test/universal/math.js
@@ -3,11 +3,11 @@ const expect = require("chai").expect;
 
 describe("math", () => {
   describe("polarity()", () => {
-    it("should be 1 with a positive number", () => {
+    it("is 1 with a positive number", () => {
       expect(math.polarity(5)).to.equal(1);
     });
 
-    it("should be -1 with a negative number", () => {
+    it("is -1 with a negative number", () => {
       expect(math.polarity(-5)).to.equal(-1);
     });
   });
