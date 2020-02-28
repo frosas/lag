56621ead03ad661e5001663d8ca642f9c61f9f95
Francesc Rosas <francescrosasbosque@gmail.com>
2020-02-28 15:51:07 +0000

    Use a single Web Worker

--- src/browser/ping/index.ts
+++ src/browser/ping/index.ts
@@ -12,6 +12,8 @@ type ConstructorParams = {
 
 let nextId = 0;
 
+let worker: Worker;
+
 export default class Ping {
   public readonly events = new EventEmitter();
   public readonly id = `${nextId++}`;
@@ -21,27 +23,34 @@ export default class Ping {
   public end?: number;
 
   constructor({ webWorkerUrl }: ConstructorParams) {
-    // TODO Use a single worker
-    const worker = new Worker(webWorkerUrl);
-    worker.addEventListener("message", event => {
+    // TODO Initialize the worker outside of the module?
+    worker = worker || new Worker(webWorkerUrl);
+    const markAsDone = () => {
+      this.done = true;
+      // eslint-disable-next-line @typescript-eslint/no-use-before-define
+      worker.removeEventListener("message", onMessage);
+    };
+    const onMessage = (event: MessageEvent) => {
       // TODO Constraint event.data type
+      if (event.data.pingId !== this.id) return;
       switch (event.data.type) {
         case "requested":
           this.start = event.data.time;
           this.events.emit("requested");
           break;
         case "responded":
-          this.done = true;
           this.end = event.data.time;
+          markAsDone();
           this.events.emit("responded");
           break;
         case "failed":
-          this.done = true;
           this.failed = true;
+          markAsDone();
           break;
       }
-    });
-    worker.postMessage({ type: "requested" });
+    };
+    worker.addEventListener("message", onMessage);
+    worker.postMessage({ type: "requested", pingId: this.id });
   }
 
   /** @returns The latest lag or null if the ping hasn't been sent yet */
--- src/browser/ping/web-worker.ts
+++ src/browser/ping/web-worker.ts
@@ -7,14 +7,15 @@ const worker: Worker = self as any;
 
 worker.onmessage = async event => {
   // TODO Constraint event.data type
+  const { pingId } = event.data;
   switch (event.data.type) {
     case "requested":
-      worker.postMessage({ type: "requested", time: Date.now() });
+      worker.postMessage({ type: "requested", pingId, time: Date.now() });
       try {
         await new Request().loaded;
-        worker.postMessage({ type: "responded", time: Date.now() });
+        worker.postMessage({ type: "responded", pingId, time: Date.now() });
       } catch (error) {
-        worker.postMessage({ type: "failed" });
+        worker.postMessage({ type: "failed", pingId });
       }
       break;
     default:
