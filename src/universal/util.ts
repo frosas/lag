1a812525b1604c275303fc3f2c0f928af79c6010
Francesc Rosas <francescrosasbosque@gmail.com>
2020-03-02 13:33:27 +0000

    Remove resumeOnThrow()
    It's unused

--- src/universal/util.ts
+++ src/universal/util.ts
@@ -11,18 +11,5 @@ export const timeout = (duration: number, promise: Promise<unknown>) =>
     })
   ]);
 
-/**
- * Resume execution flow on exception throwing
- */
-export const resumeOnThrow = (callback: Function) => {
-  try {
-    callback();
-  } catch (error) {
-    setTimeout(() => {
-      throw error;
-    });
-  }
-};
-
 // See https://reactjs.org/docs/hooks-faq.html#is-there-something-like-forceupdate
 export const useForceUpdate = () => useReducer(x => x + 1, 0)[1];
