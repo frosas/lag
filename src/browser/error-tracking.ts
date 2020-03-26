c74b3731b3a50827bca3418a238123481250bb88
Francesc Rosas <francescrosasbosque@gmail.com>
2020-03-26 12:14:19 +0000

    Don't track errors during development

--- src/browser/error-tracking.ts
+++ src/browser/error-tracking.ts
@@ -1,5 +1,10 @@
-import { init as initSentry } from "@sentry/browser";
+if (process.env.NODE_ENV === "production") {
+  const { init: initSentry } = require("@sentry/browser");
+  initSentry({
+    dsn: "https://a7399e1672354e7589a0fa0895268ca0@sentry.io/3593031"
+  });
+}
 
-initSentry({
-  dsn: "https://a7399e1672354e7589a0fa0895268ca0@sentry.io/3593031"
-});
+// Avoids the "All files must be modules when the '--isolatedModules' flag is
+// provided." error. See https://stackoverflow.com/questions/56577201/why-is-isolatedmodules-error-fixed-by-any-import
+export {};
