aecc09352c0b835f1e336e7399e489c97faea6a7
Francesc Rosas <francesc@francescrosas.com>
2024-04-14 15:53:45 +0100

    Update @frosas/eslint-config

--- eslint.config.js
+++ eslint.config.js
@@ -1,19 +1,6 @@
 const frosasConfig = require("@frosas/eslint-config")
 const globals = require("globals")
 const { config } = require("typescript-eslint")
-const reactConfigRecommended = require("eslint-plugin-react/configs/recommended")
-
-const reactConfig = config({
-  files: ["**/*.{jsx,tsx}"],
-  ...reactConfigRecommended,
-  settings: {
-    ...reactConfigRecommended.settings,
-    react: {
-      ...reactConfigRecommended.settings?.react,
-      version: "detect",
-    },
-  },
-})
 
 const jsConfig = config({
   languageOptions: { parserOptions: { ecmaVersion: 2020 } },
@@ -24,10 +11,6 @@ const configFilesConfig = config({
   languageOptions: { globals: globals.node },
 })
 
-module.exports = config(
-  ...frosasConfig,
-  ...reactConfig,
-  ...jsConfig,
-  ...configFilesConfig,
-  { ignores: ["!**/.*.js", "dist/"] },
-)
+module.exports = config(...frosasConfig, ...jsConfig, ...configFilesConfig, {
+  ignores: ["!**/.*.js", "dist/"],
+})
