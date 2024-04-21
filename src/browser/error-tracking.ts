7ed2ad6729b3fc35b8c6c9fb88957e401e644005
Francesc Rosas <francesc@francescrosas.com>
2024-04-21 22:52:51 +0100

    Update @sentry/browser

--- src/browser/error-tracking.ts
+++ src/browser/error-tracking.ts
@@ -3,5 +3,5 @@ import * as Sentry from "@sentry/browser"
 Sentry.init({
   dsn: "https://a7399e1672354e7589a0fa0895268ca0@sentry.io/3593031",
   environment: process.env.NODE_ENV,
-  autoSessionTracking: false, // Don't require the GDPR prompt
+  autoSessionTracking: false, // Avoids requiring the GDPR prompt
 })
