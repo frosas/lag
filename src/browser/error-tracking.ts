85c4eb77238a380126e36f05014a5c82bac61123
Francesc Rosas <francesc@francescrosas.com>
2025-02-22 22:19:51 +0000

    Remove autoSessionTracking
    I don't think it's needed

--- src/browser/error-tracking.ts
+++ src/browser/error-tracking.ts
@@ -3,5 +3,4 @@ import * as Sentry from "@sentry/browser"
 Sentry.init({
   dsn: "https://a7399e1672354e7589a0fa0895268ca0@sentry.io/3593031",
   environment: process.env.NODE_ENV,
-  autoSessionTracking: false, // Avoids requiring the GDPR prompt
 })
