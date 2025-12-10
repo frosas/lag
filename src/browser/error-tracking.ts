f829c97b03999107bfb3e3969801c104e2153626
Francesc Rosas <francesc@francescrosas.com>
2025-12-10 10:04:26 +0000

    Add browser tracing

--- src/browser/error-tracking.ts
+++ src/browser/error-tracking.ts
@@ -3,4 +3,6 @@ import * as Sentry from "@sentry/browser"
 Sentry.init({
   dsn: "https://a7399e1672354e7589a0fa0895268ca0@sentry.io/3593031",
   environment: process.env.NODE_ENV,
+  integrations: [Sentry.browserTracingIntegration()],
+  tracesSampleRate: 1.0,
 })
