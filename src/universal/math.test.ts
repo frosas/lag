238f09d85f4e5be2b79785da4f224d21ef70e1c6
Francesc Rosas <francesc@francescrosas.com>
2025-12-15 12:22:19 +0000

    Mocha -> Vitest

--- src/universal/math.test.ts
+++ src/universal/math.test.ts
@@ -1,14 +1,14 @@
-import { expect } from "chai"
+import { describe, expect, it } from "vitest"
 import { polarity } from "./math"
 
 describe("math", () => {
   describe("polarity()", () => {
     it("is 1 with a positive number", () => {
-      expect(polarity(5)).to.equal(1)
+      expect(polarity(5)).toBe(1)
     })
 
     it("is -1 with a negative number", () => {
-      expect(polarity(-5)).to.equal(-1)
+      expect(polarity(-5)).toBe(-1)
     })
   })
 })
